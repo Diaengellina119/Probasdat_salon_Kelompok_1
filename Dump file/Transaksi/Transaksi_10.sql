ca21eb434e3b525588b1944f7bfc8e90bd73b739
Diaengellina119 <24051214119@mhs.unesa.ac.id>
2025-10-24 10:59:12 +0700

    Add files via upload

---
+++ Dump file/Transaksi/Transaksi_10.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Transaksi" ("id_transaksi", "id_pegawai", "id_pelanggan", "kode_perawatan", "metode_pembayaran", "tanggal_transaksi") VALUES
+('PSN00001', 'PGW00001', 'PLG00001', 'PRW00001', 'Cash', '2025-01-12'),
+('PSN00002', 'PGW00003', 'PLG00004', 'PRW00007', 'Qris', '2025-02-15'),
+('PSN00003', 'PGW00005', 'PLG00002', 'PRW00003', 'Cash', '2025-03-10'),
+('PSN00004', 'PGW00002', 'PLG00005', 'PRW00010', 'Qris', '2025-04-05'),
+('PSN00005', 'PGW00004', 'PLG00007', 'PRW00005', 'Cash', '2025-04-19'),
+('PSN00006', 'PGW00006', 'PLG00003', 'PRW00009', 'Cash', '2025-05-02'),
+('PSN00007', 'PGW00007', 'PLG00006', 'PRW00004', 'Qris', '2025-05-23'),
+('PSN00008', 'PGW00008', 'PLG00008', 'PRW00002', 'Cash', '2025-06-11'),
+('PSN00009', 'PGW00009', 'PLG00009', 'PRW00006', 'Qris', '2025-07-08'),
+('PSN00010', 'PGW00010', 'PLG00010', 'PRW00008', 'Cash', '2025-08-01');
