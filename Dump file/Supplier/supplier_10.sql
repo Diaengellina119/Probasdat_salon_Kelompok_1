97d827c23d47f5b4c3d597cecf6c361b350d6ddd
Diaengellina119 <24051214119@mhs.unesa.ac.id>
2025-10-24 10:56:00 +0700

    Add files via upload

---
+++ Dump file/Supplier/supplier_10.sql
@@ -0,0 +1,11 @@
+INSERT INTO public."Supplier" ("Id_supplier", "nama_supplier", "no_telp_supplier", "alamat_supplier") VALUES
+('SPP00001', 'PT Sejahtera Kosmetik', '628096647984', 'Malang'),
+('SPP00002', 'PT Abadi Kosmetik', '628497673437', 'Semarang'),
+('SPP00003', 'PT Sejahtera Utama', '628180928792', 'Makassar'),
+('SPP00004', 'PT Gemilang Mulya', '628665419098', 'Bandung'),
+('SPP00005', 'PT Cemerlang Mulya', '628423854395', 'Semarang'),
+('SPP00006', 'PT Sentosa Mulya', '628435459168', 'Malang'),
+('SPP00007', 'PT Abadi Mulya', '628167609684', 'Surabaya'),
+('SPP00008', 'PT Cemerlang Utama', '628020960827', 'Malang'),
+('SPP00009', 'PT Mekar Kosmetik', '628195097301', 'Medan'),
+('SPP00010', 'PT Gemilang Herbal', '628811050165', 'Makassar');
