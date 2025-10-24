b5cb104bc77bcbb1ee9d860fbc9d05217f2c6801
Diaengellina119 <24051214119@mhs.unesa.ac.id>
2025-10-24 10:43:32 +0700

    Add files via upload

---
+++ Dump file/Barang/barang_10.sql
@@ -0,0 +1,11 @@
+INSERT INTO public."Barang" ("Id_barang", "nama_barang", "satuan", "Stok", "Harga_beli", "Tanggal_kadaluwarsa") VALUES
+('BRG00001', 'Shampoo', 'Pcs', 134, 148833, '2028-08-31'),
+('BRG00002', 'Lip Balm', 'Tube', 346, 443758, '2029-08-15'),
+('BRG00003', 'Loose Powder', 'Botol', 340, 242696, '2029-04-07'),
+('BRG00004', 'Face Wash', 'Sachet', 79, 46861, '2027-09-16'),
+('BRG00005', 'Toner', 'Box', 470, 250554, '2028-07-03'),
+('BRG00006', 'Facial Wash', 'Sachet', 249, 254252, '2029-02-04'),
+('BRG00007', 'Face Wash', 'Sachet', 456, 318367, '2027-12-04'),
+('BRG00008', 'Face Wash', 'Pack', 273, 103914, '2026-11-27'),
+('BRG00009', 'Body Scrub', 'Botol', 246, 469351, '2028-06-13'),
+('BRG00010', 'BB Cream', 'Pack', 41, 255232, '2030-05-19');
