9f5564dce74338a494b5a1aadf23694337d7c31c
Diaengellina119 <24051214119@mhs.unesa.ac.id>
2025-10-24 10:55:13 +0700

    Add files via upload

---
+++ Dump file/Pegawai/pegawai_10.sql
@@ -0,0 +1,11 @@
+INSERT INTO public."Pegawai" ("id_pegawai", "nama_pegawai", "usn_pegawai", "pw_pegawai", "alamat_pegawai", "telp_pegawai") VALUES
+('PGW00001', 'Andi Wijaya', 'andiwijaya@gmail.com', '65622453', 'Osaka', '629783328552'),
+('PGW00002', 'Kevin Kusuma', 'kevinkusuma@gmail.com', '41385364', 'Beijing', '629311021444'),
+('PGW00003', 'James Cahya', 'jamescahya@gmail.com', '26660081', 'Chicago', '628357038181'),
+('PGW00004', 'Zahwa Maulidatul', 'zahwamaulidatul@gmail.com', '63233807', 'Dubai', '629736164823'),
+('PGW00005', 'Fajar Haryanto', 'fajarharyanto@gmail.com', '88932368', 'Bali', '628446520706'),
+('PGW00006', 'Fajar Susanto', 'fajarsusanto@gmail.com', '92048285', 'Roma', '629692973498'),
+('PGW00007', 'Sari Permata', 'saripermata@gmail.com', '55268840', 'Berlin', '629698348349'),
+('PGW00008', 'Dewi Utami', 'dewiutami@gmail.com', '59890500', 'Milan', '628604885155'),
+('PGW00009', 'Dian Kusuma', 'diankusuma@gmail.com', '28264191', 'Osaka', '629091855298'),
+('PGW00010', 'Rizki Cahya', 'rizkicahya@gmail.com', '43386535', 'Osaka', '629437298231');
