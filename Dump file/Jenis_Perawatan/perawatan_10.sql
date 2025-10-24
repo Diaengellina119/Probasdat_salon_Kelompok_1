6cffc4310a210996a788aa27b2d5d598102a8242
Diaengellina119 <24051214119@mhs.unesa.ac.id>
2025-10-24 10:47:00 +0700

    Add files via upload

---
+++ Dump file/Jenis_Perawatan/perawatan_10.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Jenis_Perawatan" ("kode_perawatan", "nama_perawatan", "jenis_perawatan", "harga_perawatan") VALUES
+('PRW00001', 'Manicure', 'Perawatan Kuku', 248393),
+('PRW00002', 'Massage Relaksasi', 'Perawatan Tubuh', 129903),
+('PRW00003', 'Nail Art', 'Perawatan Kuku', 180453),
+('PRW00004', 'Cuticle Care', 'Perawatan Kuku', 300852),
+('PRW00005', 'Facial Gold', 'Perawatan Wajah', 209013),
+('PRW00006', 'Nail Art', 'Perawatan Kuku', 153585),
+('PRW00007', 'Hair Mask', 'Perawatan Rambut', 108385),
+('PRW00008', 'French Tips', 'Perawatan Kuku', 338397),
+('PRW00009', 'Manicure', 'Perawatan Kuku', 284817),
+('PRW00010', 'French Tips', 'Perawatan Kuku', 297569);
