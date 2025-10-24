98f8b9d4f7f8b2ab4a112e28356ca7a937f05e2a
Diaengellina119 <24051214119@mhs.unesa.ac.id>
2025-10-24 10:54:27 +0700

    Add files via upload

---
+++ Dump file/Pelanggan/pelanggan_10.sql
@@ -0,0 +1,12 @@
+INSERT INTO public."Pelanggan" 
+("id_pelanggan", "nama_pelanggan", "usn_pelanggan", "pw_pelanggan", "alamat_pelanggan", "telp_pelanggan") VALUES
+('PLG00001', 'Zahwa Maulidatul', 'zahwamaulidatul@gmail.com', '89084028', 'London', '6289166541223'),
+('PLG00002', 'Andi Pratama', 'andipratama@gmail.com', '76329184', 'Tokyo', '6289275512290'),
+('PLG00003', 'Budi Santoso', 'budisantoso@gmail.com', '21457896', 'Seoul', '6289536671984'),
+('PLG00004', 'Dewi Anggraini', 'dewianggraini@gmail.com', '93468251', 'Berlin', '6289117623450'),
+('PLG00005', 'Rina Maharani', 'rinamaharani@gmail.com', '87542190', 'Toronto', '6289287653321'),
+('PLG00006', 'Ahmad Rifai', 'ahmadrifai@gmail.com', '32874159', 'Osaka', '6289527811645'),
+('PLG00007', 'Sinta Lestari', 'sintalestari@gmail.com', '76542318', 'Paris', '6289324567812'),
+('PLG00008', 'James Halim', 'jameshalim@gmail.com', '53427691', 'Medan', '6289123459987'),
+('PLG00009', 'Putri Amanda', 'putriamanda@gmail.com', '64738291', 'Doha', '6289223345567'),
+('PLG00010', 'Daniel Gunawan', 'danielgunawan@gmail.com', '24893576', 'Sydney', '6289556671234');
